d8a04efa09b780a312666277729a7bd1f543b925
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2015-11-21 16:55:26 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,36 @@
+# eslint-plugin-microtemplates
+
+ESLint plugin for linting Resig-style microtemplate strings.
+
+## Installation
+
+You'll first need to install [ESLint](http://eslint.org):
+
+```
+$ npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-microtemplates`:
+
+```
+$ npm install eslint-plugin-microtemplates --save-dev
+```
+
+**Note:** If you installed ESLint globally (using the `-g` flag) then you must also install `eslint-plugin-microtemplates` globally.
+
+## Usage
+
+Add `microtemplates` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+    "plugins": [
+        "microtemplates"
+    ]
+}
+```
+
+
+
+
+
---
+++ tests/lib/processor.js
@@ -0,0 +1,31 @@
+/**
+ * @fileoverview Processor tests for eslint-plugin-microtemplates.
+ * @author Kevin Partington
+ * @copyright 2015 Kevin Partington. All rights reserved.
+ */
+
+//-----------------------------------------------------------------------------
+// Requirements
+//-----------------------------------------------------------------------------
+
+var assert = require("chai").assert,
+    processor = require("../../lib/processor");
+
+//-----------------------------------------------------------------------------
+// Tests
+//-----------------------------------------------------------------------------
+
+describe("preprocess", function() {
+    it("should safely wrap empty file", function() {
+        var result = processor.preprocess("");
+        assert.ok(result);
+    });
+});
+
+describe("postprocess", function() {
+    it("should unwrap empty message array", function() {
+        var messages = [[]];
+        var result = processor.postprocess(messages);
+        assert.strictEqual(result, messages[0]);
+    });
+});
