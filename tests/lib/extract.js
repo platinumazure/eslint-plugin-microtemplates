174a615535b2856588ec2e784ec3b6143c269e16
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2015-12-19 13:01:39 -0600

    Update: Simple interpolation test for extractor.

--- tests/lib/extract.js
+++ tests/lib/extract.js
@@ -24,19 +24,38 @@ var fixtures = {
 // Tests
 //-----------------------------------------------------------------------------
 
-describe("simple interpolation", function() {
-    var fileContents;
+describe("extract", function () {
+    describe("simple interpolation", function() {
+        var fileContents;
 
-    beforeEach(function(done) {
-        fs.readFile(fixtures.simpleInterpolation, "utf-8", function(err, contents) {
-            if (err) throw err;
-            fileContents = contents;
-            done();
+        beforeEach(function(done) {
+            fs.readFile(fixtures.simpleInterpolation, "utf-8", function(err, contents) {
+                if (err) throw err;
+                fileContents = contents;
+                done();
+            });
         });
-    });
 
-    it("sanity test", function() {
-        assert.ok(fileContents);
-        assert.ok(extract(fileContents));
+        it("should extract 4 code snippets", function() {
+            var result = extract(fileContents);
+
+            assert.strictEqual(result.length, 4);
+
+            assert.strictEqual(result[0].code, "/*global print*/");
+            assert.notProperty(result[0], "line");
+            assert.notProperty(result[0], "column");
+
+            assert.strictEqual(result[1].code, "print('Hello, '); // eslint-disable-line");
+            assert.notProperty(result[1], "line");
+            assert.notProperty(result[1], "column");
+
+            assert.strictEqual(result[2].code, "print( \"world!\" ); // eslint-disable-line semi");
+            assert.strictEqual(result[2].line, 1);
+            assert.strictEqual(result[2].column, 8);
+
+            assert.strictEqual(result[3].code, "print('\\r\\n'); // eslint-disable-line");
+            assert.notProperty(result[3], "line");
+            assert.notProperty(result[3], "column");
+        });
     });
 });
