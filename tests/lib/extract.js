2ae0af44d91690312a3206f9a555fe88a8a8efd6
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2015-12-26 13:07:24 -0600

    Update: extract supports interpolation on multiple lines

--- tests/lib/extract.js
+++ tests/lib/extract.js
@@ -18,7 +18,8 @@ var assert = require("chai").assert,
 
 var fixtures = {
     simpleInterpolation: "tests/fixtures/simple-interpolation.html",
-    twoInterpolationsOneLine: "tests/fixtures/two-interpolations-one-line.html"
+    twoInterpolationsOneLine: "tests/fixtures/two-interpolations-one-line.html",
+    simpleInterpolationMultiline: "tests/fixtures/simple-interpolation-multiline.html"
 };
 
 //-----------------------------------------------------------------------------
@@ -73,7 +74,6 @@ describe("extract", function () {
 
         it("should extract 5 code snippets", function() {
             var result = extract(fileContents);
-            console.log(JSON.stringify(result, null, 4));
 
             assert.strictEqual(result.length, 5);
 
@@ -92,6 +92,48 @@ describe("extract", function () {
             assert.strictEqual(result[3].code, "print( \"world!\" ); // eslint-disable-line semi");
             assert.strictEqual(result[3].originalLine, 1);
             assert.strictEqual(result[3].originalColumn, 20);
+        });
+    });
+
+    describe("simple interpolation (multiple lines of text)", function() {
+        var fileContents;
+
+        beforeEach(function(done) {
+            fs.readFile(fixtures.simpleInterpolationMultiline, "utf-8", function(err, contents) {
+                if (err) throw err;
+                fileContents = contents;
+                done();
+            });
+        });
+
+        it("should extract 6 code snippets", function() {
+            var result = extract(fileContents);
+
+            assert.strictEqual(result.length, 6);
+
+            assert.strictEqual(result[0].code, "/*global print*/");
+            assert.notProperty(result[0], "originalLine");
+            assert.notProperty(result[0], "originalColumn");
+
+            assert.strictEqual(result[1].code, "print('Hello, '); // eslint-disable-line");
+            assert.notProperty(result[1], "originalLine");
+            assert.notProperty(result[1], "originalColumn");
+
+            assert.strictEqual(result[2].code, "print( \"world!\" ); // eslint-disable-line semi");
+            assert.strictEqual(result[2].originalLine, 1);
+            assert.strictEqual(result[2].originalColumn, 11);
+
+            assert.strictEqual(result[3].code, "print('\\r\\nHow are you '); // eslint-disable-line");
+            assert.notProperty(result[3], "originalLine");
+            assert.notProperty(result[3], "originalColumn");
+
+            assert.strictEqual(result[4].code, "print( \"today\" ); // eslint-disable-line semi");
+            assert.strictEqual(result[4].originalLine, 2);
+            assert.strictEqual(result[4].originalColumn, 16);
+
+            assert.strictEqual(result[5].code, "print('?\\r\\n'); // eslint-disable-line");
+            assert.notProperty(result[5], "originalLine");
+            assert.notProperty(result[5], "originalColumn");
         });
     });
 });
