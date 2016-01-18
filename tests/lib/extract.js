2242ca2d517305c47ad687e0dff79dff48bacd7a
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2016-01-18 09:33:47 -0600

    Update: Adding range information to extracted segments.

--- tests/lib/extract.js
+++ tests/lib/extract.js
@@ -46,21 +46,25 @@ describe("extract", function () {
 
             assert.strictEqual(result.length, 4);
 
-            assert.strictEqual(result[0].code, "/*global print*/");
+            assert.strictEqual(result[0].code, "/*global print*/\n");
             assert.notProperty(result[0], "originalLine");
             assert.notProperty(result[0], "originalColumn");
+            assert.notProperty(result[0], "range");
 
-            assert.strictEqual(result[1].code, "print('Hello, '); // eslint-disable-line");
+            assert.strictEqual(result[1].code, "print('Hello, '); // eslint-disable-line\n");
             assert.notProperty(result[1], "originalLine");
             assert.notProperty(result[1], "originalColumn");
+            assert.notProperty(result[1], "range");
 
-            assert.strictEqual(result[2].code, "print( \"world!\" ); // eslint-disable-line semi");
+            assert.strictEqual(result[2].code, "print( \"world!\" ); // eslint-disable-line semi\n");
             assert.strictEqual(result[2].originalLine, 1);
             assert.strictEqual(result[2].originalColumn, 11);
+            assert.strictEqual(result[2].range, 0);
 
-            assert.strictEqual(result[3].code, "print('\\r\\n'); // eslint-disable-line");
+            assert.strictEqual(result[3].code, "print('\\r\\n'); // eslint-disable-line\n");
             assert.notProperty(result[3], "originalLine");
             assert.notProperty(result[3], "originalColumn");
+            assert.notProperty(result[3], "range");
         });
     });
 
@@ -80,21 +84,25 @@ describe("extract", function () {
 
             assert.strictEqual(result.length, 5);
 
-            assert.strictEqual(result[0].code, "/*global print*/");
+            assert.strictEqual(result[0].code, "/*global print*/\n");
             assert.notProperty(result[0], "originalLine");
             assert.notProperty(result[0], "originalColumn");
+            assert.notProperty(result[0], "range");
 
-            assert.strictEqual(result[1].code, "print( \"Hello,\" ); // eslint-disable-line semi");
+            assert.strictEqual(result[1].code, "print( \"Hello,\" ); // eslint-disable-line semi\n");
             assert.strictEqual(result[1].originalLine, 1);
             assert.strictEqual(result[1].originalColumn, 4);
+            assert.strictEqual(result[1].range, 0);
 
-            assert.strictEqual(result[2].code, "print(' '); // eslint-disable-line");
+            assert.strictEqual(result[2].code, "print(' '); // eslint-disable-line\n");
             assert.notProperty(result[2], "originalLine");
             assert.notProperty(result[2], "originalColumn");
+            assert.notProperty(result[2], "range");
 
-            assert.strictEqual(result[3].code, "print( \"world!\" ); // eslint-disable-line semi");
+            assert.strictEqual(result[3].code, "print( \"world!\" ); // eslint-disable-line semi\n");
             assert.strictEqual(result[3].originalLine, 1);
             assert.strictEqual(result[3].originalColumn, 20);
+            assert.strictEqual(result[3].range, 47);
         });
     });
 
@@ -114,29 +122,35 @@ describe("extract", function () {
 
             assert.strictEqual(result.length, 6);
 
-            assert.strictEqual(result[0].code, "/*global print*/");
+            assert.strictEqual(result[0].code, "/*global print*/\n");
             assert.notProperty(result[0], "originalLine");
             assert.notProperty(result[0], "originalColumn");
+            assert.notProperty(result[0], "range");
 
-            assert.strictEqual(result[1].code, "print('Hello, '); // eslint-disable-line");
+            assert.strictEqual(result[1].code, "print('Hello, '); // eslint-disable-line\n");
             assert.notProperty(result[1], "originalLine");
             assert.notProperty(result[1], "originalColumn");
+            assert.notProperty(result[1], "range");
 
-            assert.strictEqual(result[2].code, "print( \"world!\" ); // eslint-disable-line semi");
+            assert.strictEqual(result[2].code, "print( \"world!\" ); // eslint-disable-line semi\n");
             assert.strictEqual(result[2].originalLine, 1);
             assert.strictEqual(result[2].originalColumn, 11);
+            assert.strictEqual(result[2].range, 0);
 
-            assert.strictEqual(result[3].code, "print('\\r\\nHow are you '); // eslint-disable-line");
+            assert.strictEqual(result[3].code, "print('\\r\\nHow are you '); // eslint-disable-line\n");
             assert.notProperty(result[3], "originalLine");
             assert.notProperty(result[3], "originalColumn");
+            assert.notProperty(result[3], "range");
 
-            assert.strictEqual(result[4].code, "print( \"today\" ); // eslint-disable-line semi");
+            assert.strictEqual(result[4].code, "print( \"today\" ); // eslint-disable-line semi\n");
             assert.strictEqual(result[4].originalLine, 2);
             assert.strictEqual(result[4].originalColumn, 16);
+            assert.strictEqual(result[4].range, 47);
 
-            assert.strictEqual(result[5].code, "print('?\\r\\n'); // eslint-disable-line");
+            assert.strictEqual(result[5].code, "print('?\\r\\n'); // eslint-disable-line\n");
             assert.notProperty(result[5], "originalLine");
             assert.notProperty(result[5], "originalColumn");
+            assert.notProperty(result[5], "range");
         });
     });
 
@@ -156,21 +170,25 @@ describe("extract", function () {
 
             assert.strictEqual(result.length, 4);
 
-            assert.strictEqual(result[0].code, "/*global print*/");
+            assert.strictEqual(result[0].code, "/*global print*/\n");
             assert.notProperty(result[0], "originalLine");
             assert.notProperty(result[0], "originalColumn");
+            assert.notProperty(result[0], "range");
 
-            assert.strictEqual(result[1].code, "print('Hello, '); // eslint-disable-line");
+            assert.strictEqual(result[1].code, "print('Hello, '); // eslint-disable-line\n");
             assert.notProperty(result[1], "originalLine");
             assert.notProperty(result[1], "originalColumn");
+            assert.notProperty(result[1], "range");
 
-            assert.strictEqual(result[2].code, "print( \"world\" +\n\"!\" ); // eslint-disable-line semi");
+            assert.strictEqual(result[2].code, "print( \"world\" +\n\"!\" ); // eslint-disable-line semi\n");
             assert.strictEqual(result[2].originalLine, 1);
             assert.strictEqual(result[2].originalColumn, 11);
+            assert.strictEqual(result[2].range, 0);
 
-            assert.strictEqual(result[3].code, "print('\\n'); // eslint-disable-line");
+            assert.strictEqual(result[3].code, "print('\\n'); // eslint-disable-line\n");
             assert.notProperty(result[3], "originalLine");
             assert.notProperty(result[3], "originalColumn");
+            assert.notProperty(result[3], "range");
         });
     });
 
@@ -190,25 +208,30 @@ describe("extract", function () {
 
             assert.strictEqual(result.length, 5);
 
-            assert.strictEqual(result[0].code, "/*global print*/");
+            assert.strictEqual(result[0].code, "/*global print*/\n");
             assert.notProperty(result[0], "originalLine");
             assert.notProperty(result[0], "originalColumn");
+            assert.notProperty(result[0], "range");
 
-            assert.strictEqual(result[1].code, " for (var i = 0; i < 5; ++i) { ");
+            assert.strictEqual(result[1].code, " for (var i = 0; i < 5; ++i) { \n");
             assert.strictEqual(result[1].originalLine, 1);
             assert.strictEqual(result[1].originalColumn, 3);
+            assert.strictEqual(result[1].range, 0);
 
-            assert.strictEqual(result[2].code, "print('\\r\\nHello!\\r\\n'); // eslint-disable-line");
+            assert.strictEqual(result[2].code, "print('\\r\\nHello!\\r\\n'); // eslint-disable-line\n");
             assert.notProperty(result[2], "originalLine");
             assert.notProperty(result[2], "originalColumn");
+            assert.notProperty(result[2], "range");
 
-            assert.strictEqual(result[3].code, " } ");
+            assert.strictEqual(result[3].code, " } \n");
             assert.strictEqual(result[3].originalLine, 3);
             assert.strictEqual(result[3].originalColumn, 3);
+            assert.strictEqual(result[3].range, 32);
 
-            assert.strictEqual(result[4].code, "print('\\r\\n'); // eslint-disable-line");
+            assert.strictEqual(result[4].code, "print('\\r\\n'); // eslint-disable-line\n");
             assert.notProperty(result[4], "originalLine");
             assert.notProperty(result[4], "originalColumn");
+            assert.notProperty(result[4], "range");
         });
     });
 
@@ -228,17 +251,20 @@ describe("extract", function () {
 
             assert.strictEqual(result.length, 3);
 
-            assert.strictEqual(result[0].code, "/*global print*/");
+            assert.strictEqual(result[0].code, "/*global print*/\n");
             assert.notProperty(result[0], "originalLine");
             assert.notProperty(result[0], "originalColumn");
+            assert.notProperty(result[0], "range");
 
-            assert.strictEqual(result[1].code, " for (var i = 0; i < 5; ++i) {\r\n    print(\"Hello!\\n\");\r\n} ");
+            assert.strictEqual(result[1].code, " for (var i = 0; i < 5; ++i) {\r\n    print(\"Hello!\\n\");\r\n} \n");
             assert.strictEqual(result[1].originalLine, 1);
             assert.strictEqual(result[1].originalColumn, 3);
+            assert.strictEqual(result[1].range, 0);
 
-            assert.strictEqual(result[2].code, "print('\\r\\n'); // eslint-disable-line");
+            assert.strictEqual(result[2].code, "print('\\r\\n'); // eslint-disable-line\n");
             assert.notProperty(result[2], "originalLine");
             assert.notProperty(result[2], "originalColumn");
+            assert.notProperty(result[2], "range");
         });
     });
 });
