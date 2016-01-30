3372e5d816ab58917bd563b85067f8d41027e6f7
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2016-01-30 16:32:03 -0600

    Update: Accounting for "print(" prefix in postprocess

--- tests/integration.js
+++ tests/integration.js
@@ -17,46 +17,124 @@ var assert = require("chai").assert,
 //-----------------------------------------------------------------------------
 
 describe("Integration tests", function () {
-    var cliEngine;
 
-    beforeEach(function () {
-        cliEngine = new CLIEngine({
-            extensions: [".html"],
-            baseConfig: false,
-            rules: {
-                "semi": [2, "always"]
-            },
-            plugins: ["microtemplates"],
-            useEslintrc: false
-        });
-
-        cliEngine.addPlugin("eslint-plugin-microtemplates", pluginIndex);
-    });
-
-    describe("Basic column mapping", function () {
-        var report;
+    describe("Code execution", function () {
+        var cliEngine;
 
         beforeEach(function () {
-            report = cliEngine.executeOnFiles(["tests/fixtures/integration/known-eslint-error.html"]);
+            cliEngine = new CLIEngine({
+                extensions: [".html"],
+                baseConfig: false,
+                rules: {
+                    "semi": [2, "always"]
+                },
+                plugins: ["microtemplates"],
+                useEslintrc: false
+            });
+
+            cliEngine.addPlugin("eslint-plugin-microtemplates", pluginIndex);
         });
 
-        it("should contain results with one message", function () {
-            assert.ok(report);
-            assert.ok(report.results);
-            assert.strictEqual(report.results.length, 1);
-            assert.ok(report.results[0]);
-            assert.ok(report.results[0].messages);
-            assert.strictEqual(report.results[0].messages.length, 1);
+        describe("Error on first line", function () {
+            var report;
+
+            beforeEach(function () {
+                report = cliEngine.executeOnFiles(["tests/fixtures/integration/known-eslint-error.html"]);
+            });
+
+            it("should contain results with one message", function () {
+                assert.ok(report);
+                assert.ok(report.results);
+                assert.strictEqual(report.results.length, 1);
+                assert.ok(report.results[0]);
+                assert.ok(report.results[0].messages);
+                assert.strictEqual(report.results[0].messages.length, 1);
+            });
+
+            it("should contain correct message", function () {
+                var message = report.results[0].messages[0];
+
+                assert.ok(message);
+                assert.notOk(message.fatal);
+                assert.strictEqual(message.severity, 2);
+                assert.strictEqual(message.ruleId, "semi");
+                assert.strictEqual(message.line, 1);
+                assert.strictEqual(message.column, 10);
+            });
         });
 
-        it("should contain correct message", function () {
-            var message = report.results[0].messages[0];
+        describe("Error on second line", function () {
+            var report;
 
-            assert.notOk(message.fatal);
-            assert.strictEqual(message.severity, 2);
-            assert.strictEqual(message.ruleId, "semi");
-            assert.strictEqual(message.line, 1);
-            assert.strictEqual(message.column, 10);
+            beforeEach(function () {
+                report = cliEngine.executeOnFiles(["tests/fixtures/integration/error-multiline.html"]);
+            });
+
+            it("should contain results with one message", function () {
+                assert.ok(report);
+                assert.ok(report.results);
+                assert.strictEqual(report.results.length, 1);
+                assert.ok(report.results[0]);
+                assert.ok(report.results[0].messages);
+                assert.strictEqual(report.results[0].messages.length, 1);
+            });
+
+            it("should contain correct message", function () {
+                var message = report.results[0].messages[0];
+
+                assert.ok(message);
+                assert.notOk(message.fatal);
+                assert.strictEqual(message.severity, 2);
+                assert.strictEqual(message.ruleId, "semi");
+                assert.strictEqual(message.line, 2);
+                assert.strictEqual(message.column, 10);
+            });
+        });
+    });
+
+    describe("Expression interpolation", function () {
+        var cliEngine;
+
+        beforeEach(function () {
+            cliEngine = new CLIEngine({
+                extensions: [".html"],
+                baseConfig: false,
+                rules: {
+                    "no-extra-parens": 2
+                },
+                plugins: ["microtemplates"],
+                useEslintrc: false
+            });
+
+            cliEngine.addPlugin("eslint-plugin-microtemplates", pluginIndex);
+        });
+
+        describe("Error on first line", function () {
+            var report;
+
+            beforeEach(function () {
+                report = cliEngine.executeOnFiles(["tests/fixtures/integration/expression-error.html"]);
+            });
+
+            it("should contain results with one message", function () {
+                assert.ok(report);
+                assert.ok(report.results);
+                assert.strictEqual(report.results.length, 1);
+                assert.ok(report.results[0]);
+                assert.ok(report.results[0].messages);
+                assert.strictEqual(report.results[0].messages.length, 1);
+            });
+
+            it("should contain correct message", function () {
+                var message = report.results[0].messages[0];
+
+                assert.ok(message);
+                assert.notOk(message.fatal);
+                assert.strictEqual(message.severity, 2);
+                assert.strictEqual(message.ruleId, "no-extra-parens");
+                assert.strictEqual(message.line, 1);
+                assert.strictEqual(message.column, 5);
+            });
         });
     });
 });
